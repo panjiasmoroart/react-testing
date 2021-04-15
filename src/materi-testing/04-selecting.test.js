e13f233e9092c5e7ee5d67fd6b12181d0f9f8eae
panjiasmoroart <pandjiasmoroart@gmail.com>
2021-04-15 23:44:52 +0700

    update selecting element for action fireEvent

--- src/materi-testing/04-selecting.test.js
+++ src/materi-testing/04-selecting.test.js
@@ -7,7 +7,6 @@ describe("Testing untuk seluruh App saat pertama render", () => {
     render(<App />);
 
     const textP = screen.getByText("Saya sedang Sembunyi");
-
     expect(textP).toBeInTheDocument();
   });
 
@@ -15,7 +14,32 @@ describe("Testing untuk seluruh App saat pertama render", () => {
     render(<App />);
 
     const textButton = screen.getByText("Munculkan");
-
     expect(textButton).toBeInTheDocument();
+  });
+});
+
+// selecting element for action fireEvent (memilih element untuk memberikan sebuah aksi)
+// cara selecting ada banyak cara getByRole, getByText
+describe("Testing untuk seluruh App setelah melakukan action click pada button", () => {
+  it("memastikan kontent text P sesuai setelah action click button", () => {
+    render(<App />);
+
+    const buttonMunculkan = screen.getByText("Munculkan");
+    fireEvent.click(buttonMunculkan);
+    // screen.debug();
+
+    const textP = screen.getByText("Saya sedang Muncul");
+    expect(textP).toBeInTheDocument();
+  });
+
+  it("memastikan kontent button sesuai setelah action click button", () => {
+    render(<App />);
+
+    const button = screen.getByRole("button");
+    fireEvent.click(button);
+    // screen.debug();
+
+    const buttonSembunyikan = screen.getByText("Sembunyikan");
+    expect(buttonSembunyikan).toBeInTheDocument();
   });
 });
